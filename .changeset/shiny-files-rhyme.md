6f760a55a6685cbed983c74c5329eecfc8648718
zanminkian <hellozmj@qq.com>
2025-07-26 15:47:34 +0000

    feat: use built-in tsc path when it's fail to resolve tsc path in current directory

---
+++ .changeset/shiny-files-rhyme.md
@@ -0,0 +1,5 @@
+---
+"@rnm/tscx": patch
+---
+
+feat: use built-in tsc path when it's fail to resolve tsc path in current directory
--- packages/tscx/src/common.ts
+++ packages/tscx/src/common.ts
@@ -1,6 +1,20 @@
 import module from "node:module";
 import process from "node:process";
 
-export const tscPath = module
-  .createRequire(`${process.cwd()}/`)
-  .resolve("typescript/bin/tsc");
+function getTscPath() {
+  try {
+    return module
+      .createRequire(`${process.cwd()}/`)
+      .resolve("typescript/bin/tsc");
+  } catch (error) {
+    try {
+      return module
+        .createRequire(import.meta.url)
+        .resolve("typescript/bin/tsc");
+    } catch {
+      throw error;
+    }
+  }
+}
+
+export const tscPath = getTscPath();
